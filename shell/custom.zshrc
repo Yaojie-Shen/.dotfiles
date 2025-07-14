1785d15576ce8f9554493839c53d0f432a56d579
Yaojie Shen <acherstyx@gmail.com>
2025-07-14 13:04:53 +0800

    Update exports.sh to set cache dir, update comment

--- shell/custom.zshrc
+++ shell/custom.zshrc
@@ -1,4 +1,9 @@
-# Customization for each machine or internal environment, will not included in the public repository.
+# Customization for each machine or internal environment; will not be included in the public repository.
+#
+# The custom scripts should be placed in:
+# - ${HOME}/.persistent/custom.sh, or
+# - ${HOME}/.persistent/custom/*.sh
+# and will be sourced automatically.
 if [[ -f "${HOME}/.persistent/custom.sh" ]]; then
     source ${HOME}/.persistent/custom.sh
 elif [[ -d "${HOME}/.persistent/custom" ]]; then
--- shell/exports.sh
+++ shell/exports.sh
@@ -3,4 +3,10 @@ export HYDRA_FULL_ERROR=1
 # Set zsh history to persistent folder
 export HISTFILE="${HOME}/.persistent/.zsh_history"
 
-export HF_HOME="${HOME}/.persistent/huggingface"
+# Set huggingface cache to persistent folder
+export HF_HOME="${HOME}/.persistent/cache/huggingface"
+# Set pip cache dir to persistent folder
+export PIP_CACHE_DIR="${HOME}/.persistent/cache/pip"
+[ -d "$PIP_CACHE_DIR" ] || mkdir -p "$PIP_CACHE_DIR"
+# Set conda cache dir to persistent folder
+export CONDA_PKGS_DIRS="${HOME}/.persistent/cache/conda"
