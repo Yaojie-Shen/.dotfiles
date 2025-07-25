1ea0136e2440fd870117b38efeac1261cb29abcf
Yaojie Shen <acherstyx@gmail.com>
2025-07-25 18:00:18 +0800

    Update nvim config
    1. Set <C-s> to save file changes
    2. Add persistence.nvim

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -8,3 +8,5 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map("n", "<C-s>", ":w<CR>", { noremap = true, silent = true })
+map("i", "<C-s>", "<Esc>:w<CR>a", { noremap = true, silent = true })
