b16d62de191f80bc46fc04cee7a585c191407b61
Yaojie Shen <acherstyx@gmail.com>
2025-07-29 16:50:17 +0800

    Fix LSP config
    - Fixed accidentally overwriting the nvchad default lsp config (including key map)

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,19 @@
 require("nvchad.configs.lspconfig").defaults()
 
 local servers = { "html", "cssls", "pyright" }
+
+-- Note: change config before enable LSP server
+vim.lsp.config("pyright", {
+  on_attach = custom_on_attach,
+  settings = {
+    python = {
+      analysis = {
+        diagnosticMode = "workspace",
+      },
+    },
+  },
+})
+
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- read :h vim.lsp.config for changing options of lsp servers
