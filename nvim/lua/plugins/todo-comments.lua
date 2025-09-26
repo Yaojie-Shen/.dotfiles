c736dd5e916d4a2bcfd9cbb19e7d1f86708a0439
Yaojie Shen <acherstyx@gmail.com>
2025-09-26 16:42:57 +0800

    Update pattern for todo-comments to match TODO(author)

--- nvim/lua/plugins/todo-comments.lua
+++ nvim/lua/plugins/todo-comments.lua
@@ -1,11 +1,14 @@
 -- This plugin is used to highlight todo comments like `TODO`, `NOTE`, `WARNING` in code
 return {
-  "folke/todo-comments.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  event = "BufReadPre",
-  config = function()
-    require("todo-comments").setup {
-      -- Customize setup here
-    }
-  end
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    event = "BufReadPre",
+    config = function()
+        require("todo-comments").setup {
+            -- Highlight `TODO(author):` comments
+            -- See this issue for more details: https://github.com/folke/todo-comments.nvim/issues/10
+            search = { pattern = [[\b(KEYWORDS)(\([^\)]*\))?:]] },
+            highlight = { pattern = [[.*<((KEYWORDS)%(\(.{-1,}\))?):]] },
+        }
+    end
 }
