d46b5738a9e629d9af852faf45c9f0890fa8bc29
Yaojie Shen <acherstyx@gmail.com>
2025-11-24 11:48:30 +0800

    Add rainbow to nvim, update pytorch_conda

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -89,4 +89,15 @@ return {
       },
     },
   },
+
+  -- Rainbow
+  {
+    'HiPhish/rainbow-delimiters.nvim',
+    event = "BufReadPost",
+    config = function()
+        require('rainbow-delimiters.setup').setup {
+            -- Your configuration options here
+        }
+    end,
+  },
 }
