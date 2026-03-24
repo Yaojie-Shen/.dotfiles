cb4bca9610f761dcb91e3a36c679b405a29fefd3
Yaojie Shen <acherstyx@gmail.com>
2026-03-24 18:08:32 +0800

    Update
    Add setup script for chatgpt-cli
    
    Add setup for uv, remove deactivate alias
    
    Update installation script
    
    Fix devbox-utils print

--- shell/common/aliases.sh
+++ shell/common/aliases.sh
@@ -4,9 +4,5 @@ alias nvidia-smi-watch='watch -n 1 nvidia-smi'
 
 alias pipv="pip3 --version"
 
-alias reactivate="conda deactivate && conda activate"
-alias activate="conda activate"
-alias deactivate="conda deactivate"
-
 # SSH Config Manager
 alias sshm="ssh_manager"
